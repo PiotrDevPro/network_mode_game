00330db85e9d566538fb6864c66303c54cd8eb83
PiotrDevPro <proplaya7880281@gmail.com>
2021-03-18 20:42:49 +0300

    UI design added, swipe page added

---
+++ Assets/Scripts/UI/pageSwiper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class pageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private Vector3 panelLocation;
+    private void Start()
+    {
+        panelLocation = transform.position;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        
+        float difference = data.pressPosition.y - data.position.y;
+        transform.position = panelLocation - new Vector3(0, difference, 0);
+        Debug.Log(data.pressPosition - data.position);
+    }
+    public void OnEndDrag(PointerEventData data)
+    {
+        panelLocation = transform.position;
+    }
+}
---
+++ Assets/Scripts/UI/rangProgress.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class rangProgress : MonoBehaviour
+{
+    public Image fillRang;
+    public Text progress;
+
+    private void Update()
+    {
+        float number;
+        number  = fillRang.fillAmount * 100;
+        progress.text = number.ToString() + "/100";
+    }
+}
