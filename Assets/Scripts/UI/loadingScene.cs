438d2a8d868641e7dc4e17d3080ef4244d25ebc5
PiotrDevPro <proplaya7880281@gmail.com>
2021-03-18 15:44:51 +0300

    v0.1

---
+++ Assets/Scripts/UI/loadingScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class loadingScene : MonoBehaviour
+{
+    [Header("Загружаемая сцена")]
+    public int sceneID;
+    [Header("Обьекты")]
+    public Image loadingImg;
+
+    private void Start()
+    {
+        StartCoroutine(AsyncLoad());
+    }
+
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            loadingImg.fillAmount = progress;
+            yield return null;
+
+        }
+        
+    }
+}
